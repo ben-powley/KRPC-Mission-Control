74c9ef23ea060c5c7d4c14ead37744eb77a09f4c
Ben Powley <ben.powley97@gmail.com>
2017-02-02 21:48:15 +0000

    Added launch button

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import krpc
 #KRPC Config
 
 conn = krpc.connect(name='KRPC')
+print('CONNECTED TO SPACE CENTER')
+
 vessel = conn.space_center.active_vessel
 ref_frame = vessel.orbit.body.reference_frame
 
 met = conn.add_stream(getattr, vessel, 'met')
 altitude = conn.add_stream(getattr, vessel.flight(), 'mean_altitude')
 apoapsis = conn.add_stream(getattr, vessel.orbit, 'apoapsis_altitude')
-
-print('CONNECTED TO SPACE CENTER')
 
 #Flask Config
 
@@ -26,3 +26,13 @@ def index():
 def krpc__telem():
     if request.method == 'GET':
         return jsonify(met=round(met()), altitude=round(altitude()), apoapsis=round(apoapsis()))
+
+@app.route('/krpc/launch', methods=['GET'])
+def krpc__launch():
+    if request.method == 'GET':
+        vessel.auto_pilot.target_pitch_and_heading(90, 90)
+        vessel.auto_pilot.engage()
+        vessel.control.throttle = 1
+        vessel.control.activate_next_stage()
+
+        return jsonify(status='go')
